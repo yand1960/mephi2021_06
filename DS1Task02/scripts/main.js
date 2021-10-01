a7a7d95e235b3eaafb9d23425f6d9e0642f7851c
Denis-Savitsky <denis_savickii96@mail.ru>
2021-10-01 21:52:35 +0300

    Add project

---
+++ DS1Task02/scripts/main.js
@@ -0,0 +1,42 @@
+function showLarge(thumb) {
+    const pic_name = thumb.src;
+    const splitted = pic_name.split(".");
+    document.getElementById("large_photo").src = splitted[0] + "-large." + splitted[1];
+    for (element of document.getElementsByClassName("thumbnails")[0].children) {
+        element.classList.remove("selected")
+    }
+    thumb.classList.add("selected");
+}
+
+var images;
+var msg;
+document.addEventListener('DOMContentLoaded', () => {
+    images = document.querySelectorAll("div.thumbnails > img");
+    msg    = document.querySelector("div.thumbnails > div");
+});
+
+function fiterPhotosByName(input) {
+    if (input == "") {
+        images.forEach((img) => {
+            img.style.display = null;
+        });
+    }
+    let imageFound = false;
+    images.forEach((img) => {
+        let path = img.src.split("/")
+        const name = path.pop();
+        if (!name.startsWith(input)) {
+            img.style.display = "none";
+        } else {
+            imageFound = true;
+            img.style.display = null;
+        }
+
+    });
+
+    if (imageFound) {
+        msg.style.visibility = 'hidden';
+    } else {
+        msg.style.visibility = 'visible';
+    }
+};
