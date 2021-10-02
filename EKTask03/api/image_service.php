03cdb55bad6c174f891ec48bd3ee2d34d374f6e2
egorkaig38 <egorkaig38@gmail.com>
2021-10-02 16:09:07 +0300

    Task03

---
+++ EKTask03/api/image_service.php
@@ -0,0 +1,18 @@
+<?php
+    header("Content-Type: application/json; charset=UTF-8");
+    header("Access-Control-Allow-Origin: *");
+
+
+				$files = scandir(__DIR__."\..\images");
+				$images = [];
+
+				foreach($files as $file) {
+					if(strpos($file,"large") === FALSE and strlen($file) > 3) {
+						array_push($images,
+                        array("url" => "images/$file", "caption" => "undefined" ));
+					}
+				}
+
+               // var_dump($images);
+               sleep(0.5);
+               echo(json_encode($images));
