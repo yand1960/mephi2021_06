55b6c290ed5e7365ce3355fdbe5a316effb0759c
Irin <irina.twinkle99@gmail.com>
2021-10-02 16:04:37 +0300

    IVTask03

---
+++ IVTask03/api/image_service.php
@@ -0,0 +1,17 @@
+<?php
+    header("Content-Type: application/json; charset=UTF-8");
+    header("Access-Control-Allow-Origin: *");
+    $files = scandir(__DIR__."/../images");
+    $images = [];
+
+    $class_name = "selected";
+    foreach($files as $file) {
+        if (!strpos($file, "large") and strlen($file) > 3) {
+            array_push($images, array("url" => "images/$file", "caption" => "undefined"));
+        }
+    }
+
+    // var_dump($images);		
+
+    echo(json_encode($images));	
+			
