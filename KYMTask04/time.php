5bf044ad6eb87daf857ab8ba6cbb8446b5fc70f1
kirillmokshin <mygorod11@gmail.com>
2021-10-09 13:27:29 +0300

    Kirill Mokshin added Task04

---
+++ KYMTask04/time.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>Мой альбом</title>
+        <meta charset = "utf-8" />
+    </head>
+    <body>
+        <h1>Привет</h1>
+        <?php
+            $now = date("H:i:s");
+            echo("<h2>Страница открыта в $now</h2>");
+        ?>
+    </body>
+</html>
